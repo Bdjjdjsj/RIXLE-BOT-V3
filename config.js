92295c128435522e497ff15e693e24a5c84403c2
ElainaBot <92618649+Bdjjdjsj@users.noreply.github.com>
2021-11-13 21:26:23 +0700

    Update config.js
    T

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 userbot = {
 Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner: [ "6281261324817", "62895-6229-34562" ],
+ owner: [ "6287836855481", "62878-3685-5481" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
@@ -34,8 +34,8 @@ Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
    eror: "Sepertinya Eror",
    waits: "Sedang di proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU',
-   packname: "RIXLE-BOT-V3",
-   author: 'Arifi Razzaq',
+   packname: "ＥＬＡＩＮＡ ⲂⲟⲦz",
+   author: 'Elaina-chan',
     setting: {
     admin: "only admin",
     group: "only group",
